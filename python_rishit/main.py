133d9fbc1f579dedf52db1a7d3f2b38fba0e7861
Rishit Goel <89786498+rishit03@users.noreply.github.com>
2024-11-01 10:48:25 -0700

    Add files via upload

--- python_rishit/main.py
+++ python_rishit/main.py
@@ -7,7 +7,7 @@ xs = [[2.0, 3.0, -1.0],
       [0.5, 1.0, 1.0], 
       [1.0, 1.0, -1.0]]  # Input data
 
-ys = [0.0, 1.0, 1.0, 0.0]  # Target outputs
+ys = [0.0, 1.0, 1.0, 0.0]  # Target output
 
 # Initialize the MLP with input and output sizes
 n = MLP(3, [4, 4, 1])
